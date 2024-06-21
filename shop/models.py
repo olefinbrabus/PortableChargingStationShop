5890d83c6c5a81a85dfc7e28c27af9f904e77a8f
Oleg Filipenkov <antdiscript@MacBook-Air-Antdiscript.local>
2024-06-21 19:39:15 +0300

    add order, product api view, change order product, revert and change migrations

--- shop/models.py
+++ shop/models.py
@@ -10,7 +10,7 @@ from user.models import User
 
 def product_image_path(instance: "Product", filename: str) -> pathlib.Path:
     filename = f"{slugify(instance.name)}-{uuid4()}" + pathlib.Path(filename).suffix
-    return pathlib.Path("upload/buses/") / pathlib.Path(filename)
+    return pathlib.Path("upload/products/") / pathlib.Path(filename)
 
 
 class Company(models.Model):
@@ -24,7 +24,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     type_product = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     capacity = models.IntegerField()
     output = models.IntegerField()
     cycle_life = models.IntegerField()
@@ -39,20 +39,48 @@ class Product(models.Model):
         )]
 
     def __str__(self):
-        return f"{self.name}, {self.company}"
+        return f"{self.company.name} {self.name}"
 
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    summary_price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    @property
+    def subtotal(self):
+        order_products = ProductOrder.objects.filter(order=self)
+        price = 0
+        if order_products.exists():
+            for order_product in order_products:
+                price += order_product.price
+        return price
 
     class Meta:
         ordering = ["created_at"]
 
-
-class OrderProduct(Product):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return f"{self.created_at} {self.user.email}"
 
 
+class ProductOrder(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
+        self.price = self.product.price
+        return super(ProductOrder, self).save(
+            force_insert,
+            force_update,
+            using,
+            update_fields
+        )
+
+    def __str__(self):
+        return f"{self.product.name} в {self.order.id}"
--- shop/serializers.py
+++ shop/serializers.py
@@ -1,10 +1,59 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from shop.models import Product
+from shop.models import Product, Company, Order, ProductOrder
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = "name",
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    company = CompanySerializer(read_only=True)
 
     class Meta:
         model = Product
+        fields = (
+            "name", "type_product", "company", "price", "capacity",
+            "output", "cycle_life", "created_at", "image"
+        )
 
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ("name", "image")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    products_in_order = ProductSerializer(many=True, read_only=True, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = "__all__"
+        read_only_fields = ("created_at", "subtotal")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            products = validated_data.pop('order_products')
+            order = Order.objects.create(**validated_data)
+            for product_data in products:
+                ProductOrder.objects.create(order=order, **product_data)
+            return order
+
+
+class OrderRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+# class OrderProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = OrderProduct
+#         fields = (
+#             "name", "type_product", "company", "price", "capacity",
+#             "output", "cycle_life", "created_at", "image", "order"
+#         )
